5ba975bd54ab8d692dce0345959f7133ac807ec6
maxsolana202 <max.becerras@duocuc.cl>
2026-05-20 17:09:11 -0400

    Add files via upload

---
+++ usuario/src/main/java/com/cafeteria/usuario/dto/UsuarioResponseDTO.java
@@ -0,0 +1,19 @@
+package com.cafeteria.usuario.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioResponseDTO {
+
+    private Long id;
+    private String nombre;
+    private String email;
+    private String telefono;
+    private String direccion;
+    private String rol;
+    private Boolean activo;
+}
