161237375c99b71bfc2f5aaa4af8274342e629a0
f3rnn6 <f3rnn6@gmail.com>
2026-06-19 05:54:59 -0400

    Delete mesas-service directory

--- mesas-service/src/main/java/com/cafe/mesas_service/repository/MesaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cafe.mesas_service.repository;
-
-import com.cafe.mesas_service.model.Mesa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MesaRepository extends JpaRepository<Mesa, Long> {
-}
--- mesas-service/src/main/resources/db/migration/V1__crear_tablas.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE mesas (
-                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                       numero_mesa INT NOT NULL,
-                       capacidad INT NOT NULL,
-                       estado VARCHAR(255) NOT NULL
-);
