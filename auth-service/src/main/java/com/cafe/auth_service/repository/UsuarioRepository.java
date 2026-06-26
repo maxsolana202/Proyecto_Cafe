00bdebefbbbb08d0aa9a27ee16fc292c46197a7f
maxsolana202 <max.becerras@duocuc.cl>
2026-06-26 15:22:10 -0400

    Delete auth-service directory

--- auth-service/src/main/java/com/cafe/auth_service/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cafe.auth_service.repository;
-
-import com.cafe.auth_service.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
